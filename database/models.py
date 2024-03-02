dc6181f288226ba580e15888e270846e41c3a3ff
k-sano-kwatz <k.sano@kwatz.jp>
2024-03-02 22:15:14 +0900

    Add NOT NULL constraint

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    hashed_password = Column(String)
-    email = Column(String, unique=True)
-    is_admin = Column(Boolean)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    is_admin = Column(Boolean, nullable=False)
